4f3a91e1fa5d3e558c8053d49cd90dde9006ed7b
Brian Hansen <bhansen0057@myduneland.org>
2020-09-15 08:42:28 -0500

    Initial sharing of project

---
+++ Lion.java
@@ -0,0 +1,9 @@
+public class Lion
+{
+	private int age;
+
+	public void roar()
+	{
+		System.out.println("Roar!  Roar!");
+	} // roar() end
+} //Lion end
---
+++ ZooProgram.java
@@ -0,0 +1,9 @@
+
+public class ZooProgram 
+{
+  	public static void main (String [] args) {
+   		Lion leo = new Lion ();
+		leo.roar();
+	} // main () end
+} // ZooProgram end
+
